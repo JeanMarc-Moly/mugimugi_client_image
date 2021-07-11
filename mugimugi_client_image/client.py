a233a54a00e1e4b96f9b6bad153d2845d89cacde
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-07-11 14:50:52 +0200

    properly handle timeout

--- mugimugi_client_image/client.py
+++ mugimugi_client_image/client.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from asyncio import FIRST_COMPLETED, Task, create_task, wait
 from enum import Enum
 from pathlib import Path
 from types import TracebackType
@@ -15,7 +14,7 @@ from typing import (
 
 from httpx import AsyncClient
 
-from ._util import asynchronize
+from ._util import asynchronize, execute_in_pool
 from .constant import Constant
 
 StrPath = Union[str, Path]
@@ -32,6 +31,7 @@ class MugiMugiImageClient(AsyncContextManager):
     API: ClassVar[AsyncClient] = AsyncClient(base_url=Constant.API_PATH)
     MODULO: ClassVar[int] = Constant.IMAGE_MODULO
     PARALLEL: ClassVar[int] = Constant.PARALLEL_DOWNLOAD_COUNT
+    RETRY: ClassVar[int] = Constant.FAILURE_RETRY
 
     @classmethod
     def get_url(cls, id_: int, size: Size):
@@ -49,49 +49,21 @@ class MugiMugiImageClient(AsyncContextManager):
 
     @classmethod
     async def get_many(
-        cls, ids: GreaterIntIterable, size: Size = Size.BIG, parallel: int = PARALLEL,
+        cls, ids: GreaterIntIterable, size: Size = Size.BIG,
     ) -> AsyncIterator[tuple[int, bytes]]:
+        async def _get(id_: int) -> tuple[int, bytes]:
+            return id_, await cls.get(id_, size)
 
-        get = cls.get
-
-        async def _get(id_: int):
-            return id_, await get(id_, size)
-
-        remaining_tasks: set[Task] = set()
-        completed_tasks: set[Task] = set()
-
-        async def yield_and_wait():
-            nonlocal completed_tasks
-            nonlocal remaining_tasks
-
-            for t in completed_tasks:
-                yield t.result()
-
-            (completed_tasks, remaining_tasks) = await wait(
-                remaining_tasks, return_when=FIRST_COMPLETED
-            )
-
-        async for id_ in asynchronize(ids):
-            remaining_tasks.add(create_task(_get(id_)))
-
-            if not (parallel := parallel - 1):
-                async for r in yield_and_wait():
-                    yield r
-                parallel += len(completed_tasks)
-
-        while remaining_tasks:
-            async for r in yield_and_wait():
-                yield r
-
-        for t in completed_tasks:
-            yield t.result()
+        async for int_bytes in execute_in_pool(
+            _get, ids, cls.PARALLEL, TimeoutError, cls.RETRY
+        ):
+            yield int_bytes
 
     @classmethod
     async def save_many(
         cls,
         images: Union[Iterable[Saver], AsyncIterable[Saver]],
         size: Size = Size.BIG,
-        parallel: int = PARALLEL,
     ) -> dict[int, Path]:
         saved = {}
 
@@ -102,7 +74,7 @@ class MugiMugiImageClient(AsyncContextManager):
                 yield id_
 
         # TODO: handle async images generator slower than image retrieval
-        async for id_, raw in cls.get_many(dictionize(), size, parallel):
+        async for id_, raw in cls.get_many(dictionize(), size):
             with saved[id_].open("wb") as f:
                 f.write(raw)
 
